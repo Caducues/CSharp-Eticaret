ec39f19f4c3644534abff833aa8eb5b5bad0e212
Taha Akyüz <h3.caduceus@gmail.com>
2024-12-23 02:48:03 +0300

    Sitenin Frontend Kısmı

---
+++ Test-Eticaret/Controllers/WebsiteController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Eticaret.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+        public IActionResult main()
+        {
+            return View();
+        }
+        public IActionResult signup()
+        {
+            return View();
+        }
+        public IActionResult anime_details()
+        {
+            return View();
+        }
+        public IActionResult anime_watching()
+        {
+            return View();
+        }
+        public IActionResult blog()
+        {
+            return View();
+        }
+        public IActionResult blog_details()
+        {
+            return View();
+        }
+        public IActionResult categories()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+   
+    }
+}
