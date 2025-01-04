ea7f7a43322d31a4d0cf7ad8ec08bd2eb063ef89
Taha Akyüz <h3.caduceus@gmail.com>
2025-01-04 22:50:59 +0300

    Viewcomponent ve partialview eklendi.

--- Test-Eticaret/Controllers/AdminController.cs
+++ Test-Eticaret/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Test_Eticaret.Data;
+using Test_Eticaret.Migrations;
+using Test_Eticaret.Models;
+using XAct.Users;
 
 namespace Test_Eticaret.Controllers
 {
@@ -48,8 +51,50 @@ namespace Test_Eticaret.Controllers
             return View(movies); // Veriyi view'a gönderiyoruz
         }
 
-		
+
+        [HttpGet]
+        public IActionResult addmovie()
+        {
+            return View();
+        }
 
 
-	}
+        [HttpPost]
+        public async Task<IActionResult> AddMovie(Movie movie, IFormFile picture_url, IFormFile movie_url)
+        {
+            if (ModelState.IsValid)
+            {
+                // Resmi kaydetme işlemi
+                if (picture_url != null)
+                {
+                    var picturePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", Path.GetFileName(picture_url.FileName));
+                    using (var stream = new FileStream(picturePath, FileMode.Create))
+                    {
+                        await picture_url.CopyToAsync(stream);
+                    }
+                    movie.picture_url = "/images/" + Path.GetFileName(picture_url.FileName); // Dosyanın URL'si
+                }
+
+                // Video kaydetme işlemi
+                if (movie_url != null)
+                {
+                    var videoPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/videos", Path.GetFileName(movie_url.FileName));
+                    using (var stream = new FileStream(videoPath, FileMode.Create))
+                    {
+                        await movie_url.CopyToAsync(stream);
+                    }
+                    movie.movie_url = "/videos/" + Path.GetFileName(movie_url.FileName); // Dosyanın URL'si
+                }
+
+                // Filmi veritabanına kaydetme
+                _context.Movies.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            // Form geçerli değilse, hata mesajları ile birlikte formu tekrar göster
+            return View(movie);
+        }
+
+    }
 }
