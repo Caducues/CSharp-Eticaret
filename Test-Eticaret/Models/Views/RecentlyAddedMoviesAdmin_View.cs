e531def2c63b713a83463734c3537affd5345347
ELİF DERE <222803018@ogr.cbu.edu.tr>
2025-01-06 06:51:36 +0300

    Database viewları ekleme

---
+++ Test-Eticaret/Models/Views/RecentlyAddedMoviesAdmin_View.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_Eticaret.Models.Views
+{
+    public class RecentlyAddedMoviesAdmin_View
+    {
+        public string movie_name { get; set; }
+        public int movie_id{ get; set; }
+        public DateTime add_date { get; set; }
+        
+    }
+}
